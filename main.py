6e7ddad2caf4e08c3eeec849617c6f533834afeb
Nuke22 <yura.nuke22@gmail.com>
2023-05-14 22:23:33 +0300

    final result

--- main.py
+++ main.py
@@ -1,34 +1,26 @@
-def choose_swap (swaps_allowed = 3):
-    add_swap = True
+def choose_swap ():
     swap_array = []
     swaps_done = 1
-    # rewrite this annoyance!
-    while add_swap and swaps_done <= swaps_allowed:
-        keep_swaping = input(f"\nDo you want to add aditional letter change? ({swaps_done}/{swaps_allowed}) (y/n): ")
-        
-        if keep_swaping.lower() == "yes" or keep_swaping.lower() == "y":
-            swap_pair = input("Please enter what pair of letter you want to swap? (Remember, you can not mention the same letter twice!): ")
-            first_swap_letter = swap_pair[0].upper()
-            last_swap_letter = swap_pair[-1].upper()
-            #check if the input contains only letters
-            if first_swap_letter.isalpha() and last_swap_letter.isalpha():
-                print(f"\nThe first letter is {first_swap_letter}, \nThe second one is {last_swap_letter}\n")
-                
-                #check if the input does not contain duplicates
-                if first_swap_letter in swap_array or last_swap_letter in swap_array or first_swap_letter == last_swap_letter:
-                    print("Error! You can not mention the same letter twice!")
-                else:
-                    swap_array.append(first_swap_letter)
-                    swap_array.append(last_swap_letter)
-                    swaps_done += 1
-                    print(f"These letters will be swapped {swap_array}")
-            else: 
-                print("At least one of the elements is not a character! Discarding this input. Try again \n")
-
-        else:
-            add_swap = False
-            break
-    print("No more swaps available!")
+    swaps_amount = int(input("How many latter swaps do you want to perform?: "))
+    for i in range(0, swaps_amount):
+        swap_pair = input(f"Please enter what pair of letter you want to swap? ({swaps_done}/{swaps_amount}) (Remember, you can not mention the same letter twice!): ")
+        first_swap_letter = swap_pair[0].upper()
+        last_swap_letter = swap_pair[-1].upper()
+        #check if the input contains only letters
+        if first_swap_letter.isalpha() and last_swap_letter.isalpha():
+            print(f"\nThe first letter is {first_swap_letter}, \nThe second one is {last_swap_letter}\n")
+            
+            #check if the input does not contain duplicates
+            if first_swap_letter in swap_array or last_swap_letter in swap_array or first_swap_letter == last_swap_letter:
+                print("Error! You can not mention the same letter twice!")
+            else:
+                swap_array.append(first_swap_letter)
+                swap_array.append(last_swap_letter)
+                swaps_done += 1
+                print(f"These letters will be swapped {swap_array}")
+        else: 
+            print("At least one of the elements is not a character! Discarding this input. Try again \n")
+ 
     return swap_array
 
 def choose_rotors ():
@@ -92,38 +84,45 @@ def get_rotors_with_offset (rotors, offsets):
         final_set_of_rotors.append(actual_rotor_with_offset)
     return final_set_of_rotors
    
-def encrypt (message):  
-    #transform message into numbers due to new alphabet
+def message_to_decimal (message):
     message_decimal = []
     for i in range(0, len(message)):
         letter = message[i]
         letter_index = swaped_alphabet.index(letter)
         message_decimal.append(letter_index)
-    print(f"message_decimal = {message_decimal}")
+    # print(f"message_decimal = {message_decimal}")
+    return message_decimal
+
+def decimal_to_message (decimal):
+    mesage_list = []
+    for i in range(0, len(decimal)):
+        pos = decimal[i]
+        letter = swaped_alphabet[pos]
+        mesage_list.append(letter)
+    message = "".join(mesage_list)
+    # print(f"message = {message}")
+    return message
+
+def encrypt (message):  
+    #transform message into numbers due to new alphabet
+    message_decimal = message_to_decimal(message)
     
     #pass every other letter through wheels
     i = 0
     click0 = 0
     click1 = 0
     click2 = 0
-    encripted_message_decimal = []
-    # imitating rotation of rotors
+    encrypted_message_decimal = []
     
-    #debug - clear
-    final_position_of_rotors = get_rotors_with_offset(rotors, offsets)
-    print(f"\n\nstarting array = {final_position_of_rotors}\n")
-    #debug - clear
+    # imitating rotation of rotors
     while i < len(message):
         click0 = i % 26
         click1 = i // 26
         click2 = i // 676
         final_position_of_rotors = get_rotors_with_offset(rotors, offsets)
         final_position_of_rotors[0] = final_position_of_rotors[0][click0:] + final_position_of_rotors[0][:click0]
-        print(f"final position of rotor 0 = {final_position_of_rotors[0]}")
         final_position_of_rotors[1] = final_position_of_rotors[1][click1:] + final_position_of_rotors[1][:click1]
-        print(f"final position of rotor 1 = {final_position_of_rotors[1]}")
         final_position_of_rotors[2] = final_position_of_rotors[2][click2:] + final_position_of_rotors[2][:click2]
-        print(f"final position of rotor 2 = {final_position_of_rotors[2]}")
         
         step1 = message_decimal[i]          
         step2 = final_position_of_rotors[0][step1]          
@@ -133,29 +132,52 @@ def encrypt (message):
         step6 = final_position_of_rotors[2][step5]          
         step7 = final_position_of_rotors[1][step6]          
         step8 = final_position_of_rotors[0][step7]          
-        print(f"step1 = {step1} \nstep2 = {step2}\nstep3 = {step3}\nstep4 = {step4}\nstep5 = {step5}\nstep6 = {step6}\nstep7 = {step7}\nstep8 = {step8}")
-        encripted_message_decimal.append(step8)
+        # print(f"step1 = {step1} \nstep2 = {step2}\nstep3 = {step3}\nstep4 = {step4}\nstep5 = {step5}\nstep6 = {step6}\nstep7 = {step7}\nstep8 = {step8}\n")
+        encrypted_message_decimal.append(step8)
         i += 1
-    print(f"encrypted_mesage_decimal = {encripted_message_decimal}")
-    #still flawlwss
+    print(f"encrypted_mesage_decimal = {encrypted_message_decimal}")
     
                 
-    # decypher them due to (new or old) alphabet ---------------------------------------------------------
-    encrypted_mesage_list = []
-    for i in range(0, len(encripted_message_decimal)):
-        pos = encripted_message_decimal[i]
-        letter = swaped_alphabet[pos]
-        encrypted_mesage_list.append(letter)
-    encrypted_mesage = "".join(encrypted_mesage_list)
-    print(f"encrypted message = {encrypted_mesage}")
-    return encrypted_mesage
+    # decypher them due to new alphabet
+    answer = decimal_to_message(encrypted_message_decimal)
+    return answer
 
- 
 def decrypt (messpassage):
-    swaped_alphabet = get_swapped_alphabet(alphabet, swap)
+    # transform message into numbers due to new alphabet
+    message_decimal = message_to_decimal(message)
     
+    i = 0
+    click0 = 0
+    click1 = 0
+    click2 = 0
+    decrypted_message_decimal = []
     
+    # imitating rotation of rotors
+    while i < len(message):
+        click0 = i % 26
+        click1 = i // 26
+        click2 = i // 676
+        final_position_of_rotors = get_rotors_with_offset(rotors, offsets)
+        final_position_of_rotors[0] = final_position_of_rotors[0][click0:] + final_position_of_rotors[0][:click0]
+        final_position_of_rotors[1] = final_position_of_rotors[1][click1:] + final_position_of_rotors[1][:click1]
+        final_position_of_rotors[2] = final_position_of_rotors[2][click2:] + final_position_of_rotors[2][:click2]
+                
+        step1 = message_decimal[i]
+        step2 = final_position_of_rotors[0].index(step1)
+        step3 = final_position_of_rotors[1].index(step2)
+        step4 = final_position_of_rotors[2].index(step3)
+        step5 = connector.index(step4)
+        step6 = final_position_of_rotors[2].index(step5)
+        step7 = final_position_of_rotors[1].index(step6)
+        step8 = final_position_of_rotors[0].index(step7)
+        # print(f"step1 = {step1} \nstep2 = {step2}\nstep3 = {step3}\nstep4 = {step4}\nstep5 = {step5}\nstep6 = {step6}\nstep7 = {step7}\nstep8 = {step8}")
+        decrypted_message_decimal.append(step8)
+        i += 1
+    print(f"decrypted_mesage_decimal = {decrypted_message_decimal}")
     
+    # decypher them due to new alphabet 
+    answer = decimal_to_message(decrypted_message_decimal)
+    return answer
     
 # dataset
 alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -167,17 +189,14 @@ rotor4_initial = [12, 19, 24, 0, 2, 23, 20, 3, 6, 18, 25, 8, 9, 21, 5, 16, 7, 10
 rotor5_initial = [16, 8, 6, 20, 10, 18, 3, 9, 0, 14, 11, 19, 5, 12, 21, 1, 25, 7, 22, 15, 13, 23, 2, 24, 17, 4]
 
 # start settings
-# swap = choose_swap()
-# rotors = choose_rotors()
-# offsets = choose_offsets()
-# print(f"\nSetup is complete! \nswap   = {swap}\nrotors = {rotors}\noffsets = {offsets}")
-
-swap = ["A", "B"]
-rotors = [1, 2, 3]
-offsets = [0, 0, 0]
+swap = choose_swap()
+rotors = choose_rotors()
+offsets = choose_offsets()
 print(f"\nSetup is complete! \nswap   = {swap}\nrotors = {rotors}\noffsets = {offsets}")
+
 swaped_alphabet = get_swapped_alphabet(alphabet, swap) 
-print(f"Swaped alphabet = {swaped_alphabet}") 
+# print(f"Swaped alphabet = {swaped_alphabet}") 
+
 # main
 while True:
     process = input("\nEncrypt or decrypt? (e/d): ")
@@ -193,7 +212,8 @@ while True:
         print("You are in the decription mode!")    
         message = input("Enter a message you want to decrypt:").upper().strip()
         print(f"Your message is: {message}")
-        decrypt(message)
+        result = decrypt(message)
+        print(f"Your decrypted mesage is:--------------------------- {result}")
         
     else: 
         quit = input("Input is not valid! Do you want to quit? All settings will be discarded... (y/n): ")
